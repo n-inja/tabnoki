fb89100a1dc661871dbdcc429e6fecb29ce7fcc8
g2xpf <nameko.jack@gmail.com>
2020-06-16 16:06:00 +0900

    show all tabs info

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  chrome.tabs.query({}, console.log);
+  const [tabs, setter] = useState([] as chrome.tabs.Tab[]);
+  chrome.tabs.query({ currentWindow: true }, (result) => {
+    setter(result);
+  });
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello!</p>
+        <ul>
+          {tabs.map((tab) => {
+            return <li key={tab.id}>{tab.url}</li>;
+          })}
+        </ul>
       </header>
     </div>
   );
