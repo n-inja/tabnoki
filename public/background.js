4c41b405a9ab658d63671e652828b744eb10dac9
ryotaro.kasuga <krkr9893@gmail.com>
2020-06-23 15:02:41 +0900

    get tabID to openerTabID

--- public/background.js
+++ public/background.js
@@ -1,3 +1,11 @@
+const tabID2openerID = {};
+
 chrome.browserAction.onClicked.addListener(function () {
-  chrome.tabs.create({ url: chrome.runtime.getURL("index.html") });
+  const data = JSON.stringify(tabID2openerID);
+  const s = encodeURIComponent(data);
+  chrome.tabs.create({ url: chrome.runtime.getURL("index.html") + "?q=" + s });
+});
+
+chrome.tabs.onCreated.addListener(function (tab) {
+  tabID2openerID[tab.id] = tab.openerTabId ?? -1;
 });
