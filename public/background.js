56ba4fe3cc9eb749c19620c9854b5cf35a37f92c
spaspa <yoyamssk@gmail.com>
2020-06-30 16:27:32 +0900

    add hook for tab removing

--- public/background.js
+++ public/background.js
@@ -1,11 +1,32 @@
-const tabID2openerID = {};
+const tabID2Parent = {};
+
+/** @type Record<number, number[]> */
+const tabId2Children = {};
 
 chrome.browserAction.onClicked.addListener(function () {
-  const data = JSON.stringify(tabID2openerID);
+  const data = JSON.stringify(tabID2Parent);
   const s = encodeURIComponent(data);
   chrome.tabs.create({ url: chrome.runtime.getURL("index.html") + "?q=" + s });
 });
 
 chrome.tabs.onCreated.addListener(function (tab) {
-  tabID2openerID[tab.id] = tab.openerTabId ?? -1;
+  if (tab.id === undefined) return;
+
+  tabID2Parent[tab.id] = tab.openerTabId ?? -1;
+  if (tab.openerTabId !== undefined) {
+    if (!tabId2Children[tab.openerTabId]) {
+      tabId2Children[tab.openerTabId] = [];
+    }
+    tabId2Children[tab.openerTabId].push(tab.id);
+  }
+});
+
+chrome.tabs.onRemoved.addListener(function (tabId) {
+  const openerId = tabID2Parent[tabId];
+  tabId2Children[openerId] = tabId2Children[openerId]?.filter(
+    (id) => id !== tabId
+  );
+  tabId2Children[tabId]?.forEach((id) => (tabID2Parent[id] = -1));
+  delete tabID2Parent[tabId];
+  delete tabId2Children[tabId];
 });
