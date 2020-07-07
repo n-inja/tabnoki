ce2cab7084469691e608982d52d7e87c66457e89
spaspa <yoyamssk@gmail.com>
2020-07-07 17:04:27 +0900

    icon

--- src/components/TabNode.tsx
+++ src/components/TabNode.tsx
@@ -2,19 +2,32 @@ import React, { useState, useEffect } from "react";
 import promisify from "../utils/promisify";
 import { Node } from "../tabDependent";
 import ListItem from "@material-ui/core/ListItem";
+import IconButton from "@material-ui/core/IconButton";
 import ListItemText from "@material-ui/core/ListItemText";
 import Collapse from "@material-ui/core/Collapse";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ExpandLess from "@material-ui/icons/ExpandLess";
 import ExpandMore from "@material-ui/icons/ExpandMore";
-import TabNodeList from "./TabNodeList";
-import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import Language from "@material-ui/icons/Language";
+import TabNodeList from "./TabNodeList";
 import { removeTab } from "../utils/tab";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
 
 type Props = {
   node: Node;
 };
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    inactive: {
+      opacity: 0.5,
+    },
+    text: {
+      marginLeft: "1rem",
+    },
+  })
+);
 
 export default function TabNode(props: Props) {
   const [tab, setter] = useState(undefined as chrome.tabs.Tab | undefined);
@@ -33,11 +46,20 @@ export default function TabNode(props: Props) {
 
   const hasChild = props.node.children.length > 0;
 
+  const classes = useStyles();
   return (
     <div>
       <ListItem button onClick={handleClick}>
-        {hasChild && (open ? <ExpandLess /> : <ExpandMore />)}
-        <ListItemText primary={(tab?.id ?? "") + "-" + (tab?.title ?? "")} />
+        {hasChild ? (
+          open ? (
+            <ExpandLess />
+          ) : (
+            <ExpandMore />
+          )
+        ) : (
+          <Language className={classes.inactive} />
+        )}
+        <ListItemText className={classes.text} primary={tab?.title ?? ""} />
         <ListItemSecondaryAction>
           <div onClick={() => removeTab(props.node)}>
             <IconButton edge="end" aria-label="delete">
